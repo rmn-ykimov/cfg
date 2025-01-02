10f9f29bda010921ba6470a65ce08ab4c1720f85
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2025-01-02 16:21:27 +0100

    update home manager version

--- nix-darwin/modules/home.nix
+++ nix-darwin/modules/home.nix
@@ -1,13 +1,17 @@
 { config, pkgs, ... }:
 
 {
-  home.stateVersion = "25.05";
   
-  home.username = "roman";
-  home.homeDirectory = pkgs.lib.mkForce "/Users/roman";
-  
-  home.packages = with pkgs; [
-  ];
+  home = {
+    stateVersion = "25.05";
+    username = "roman";
+    homeDirectory = pkgs.lib.mkForce "/Users/roman";
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 
   programs.home-manager.enable = true;
 }
