ec71b00219bc37999087835da2391e1942405f3e
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2025-01-03 15:05:20 +0100

    move starship config to separate modules

---
+++ nix-darwin/modules/home-modules/zsh.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -lah";
+      h = "history";
+    };
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    enableCompletion = true;
+  };
+}
