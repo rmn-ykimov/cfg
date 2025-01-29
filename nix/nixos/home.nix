f6f03b1985d37e449f7f00a2f90b7f0b53384310
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2025-01-29 18:15:35 +0100

    refactoring

--- nix/nixos/home.nix
+++ nix/nixos/home.nix
@@ -87,38 +87,38 @@ in {
       syntaxHighlighting.enable = true;
     };
 
-    git = {
-      enable = true;
-      package = pkgs.unstable.git;
-      delta.enable = true;
-      delta.options = {
-        line-numbers = true;
-        side-by-side = true;
-        navigate = true;
-      };
-      userEmail = ""; # FIXME: set your git email
-      userName = ""; #FIXME: set your git username
-      extraConfig = {
-        # FIXME: uncomment the next lines if you want to be able to clone private https repos
-        # url = {
-        #   "https://oauth2:${secrets.github_token}@github.com" = {
-        #     insteadOf = "https://github.com";
-        #   };
-        #   "https://oauth2:${secrets.gitlab_token}@gitlab.com" = {
-        #     insteadOf = "https://gitlab.com";
-        #   };
-        # };
-        push = {
-          default = "current";
-          autoSetupRemote = true;
-        };
-        merge = {
-          conflictstyle = "diff3";
-        };
-        diff = {
-          colorMoved = "default";
-        };
-      };
-    };
+    # git = {
+    #   enable = true;
+    #   package = pkgs.unstable.git;
+    #   delta.enable = true;
+    #   delta.options = {
+    #     line-numbers = true;
+    #     side-by-side = true;
+    #     navigate = true;
+    #   };
+    #   userEmail = ""; # FIXME: set your git email
+    #   userName = ""; #FIXME: set your git username
+    #   extraConfig = {
+    #     # FIXME: uncomment the next lines if you want to be able to clone private https repos
+    #     # url = {
+    #     #   "https://oauth2:${secrets.github_token}@github.com" = {
+    #     #     insteadOf = "https://github.com";
+    #     #   };
+    #     #   "https://oauth2:${secrets.gitlab_token}@gitlab.com" = {
+    #     #     insteadOf = "https://gitlab.com";
+    #     #   };
+    #     # };
+    #     push = {
+    #       default = "current";
+    #       autoSetupRemote = true;
+    #     };
+    #     merge = {
+    #       conflictstyle = "diff3";
+    #     };
+    #     diff = {
+    #       colorMoved = "default";
+    #     };
+    #   };
+    # };
   };
 }
