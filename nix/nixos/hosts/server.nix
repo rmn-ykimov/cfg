7b320402ea4fcd43ae90873a1fa35fb2f94f03c9
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2025-03-02 11:50:49 +0100

    add server config

--- nix/nixos/hosts/server.nix
+++ nix/nixos/hosts/server.nix
@@ -1,5 +1,17 @@
 {
+  system.stateVersion = "24.11";
+
+  fileSystems."/" = {
+    device = "/dev/vda1";
+    fsType = "ext4";
+  };
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "/dev/vda" ];
+
   services.cloud-init.enable = true;
   services.cloud-init.network.enable = true;
-}
 
+  networking.useDHCP = false;
+
+}
