1e2ad5c7bfd539f43b8bc7ef39c237fb48898595
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2025-03-02 12:08:50 +0100

    add server config

--- nix/nixos/hosts/server.nix
+++ nix/nixos/hosts/server.nix
@@ -9,9 +9,15 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.devices = [ "/dev/vda" ];
 
+  systemd.network.wait-online.enable = false;
+
+  networking.useDHCP = false;
+
   services.cloud-init.enable = true;
   services.cloud-init.network.enable = true;
 
-  networking.useDHCP = false;
+  services.cloud-init.settings.datasource_list = [ "ConfigDrive" ];
+
+  services.cloud-init.settings.boot.wait_for_network = { enabled = false; };
 
 }
