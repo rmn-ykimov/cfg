fb7c627e5c4994a10fb94a7ca651a47759b3f26c
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2025-06-10 14:37:54 +0200

    Add NixOS flake.

---
+++ nix/nixos/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "NixOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
