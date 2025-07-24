562b889008e7b35a4a62d15f5b74ae0f0f643076
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2025-07-24 11:40:54 +0200

    Switch nixos and wsl to 25.05.

--- nix/wsl/flake.nix
+++ nix/wsl/flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager.url = "github:nix-community/home-manager/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+    
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nur.url = "github:nix-community/NUR";
